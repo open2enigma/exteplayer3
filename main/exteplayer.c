870ec7950d2c94d0085bdd832eb254af5a6de1e5
open2enigma <post25@yahoo.com>
2025-03-20 12:17:12 +0100

    Update exteplayer.c

--- main/exteplayer.c
+++ main/exteplayer.c
@@ -756,7 +756,7 @@ int main(int argc, char* argv[])
     int commandRetVal = -1;
 
     /* inform client that we can handle additional commands */
-    E2iSendMsg("{\"EPLAYER3_EXTENDED\":{\"version\":%d}}\n", 177);
+    E2iSendMsg("{\"EPLAYER3_EXTENDED\":{\"version\":%d}}\n", 71);
 
     PlayFiles_t playbackFiles;
     memset(&playbackFiles, 0x00, sizeof(playbackFiles));
